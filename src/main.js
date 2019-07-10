e17aa30f60b3e61f8334b2311735ec60db767074
Regina Pfeifer <regina@mailbox.org>
2019-07-10 21:11:26 +0200

    Add index and 404 pages

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import 'bulma/bulma.sass'; // eslint-disable-line import/no-unassigned-import
 import App from './App.svelte';
 
+import Index from './routes/index.svelte';
+import NotFound from './routes/404.svelte';
+
 // In case we have to deliver the 404.html
 const redirect_href = sessionStorage.getItem('redirect_href');
 sessionStorage.removeItem('redirect_href');
@@ -10,9 +13,10 @@ if (redirect_href && redirect_href !== location.href) {
 
 const app = new App({
 	target: document.body,
-	props: {
-		name: location.href,
-	},
 });
 
-export default app;
+if (location.pathname === '/') {
+	app.$set({page: Index});
+} else {
+	app.$set({page: NotFound});
+}
