f2755223e92050273a04b2ff8ca6bc65c68f4308
Regina Pfeifer <regina@mailbox.org>
2019-08-03 22:36:17 +0200

    Add some content to profile page

--- src/routes/profile.svelte
+++ src/routes/profile.svelte
@@ -1,1 +1,68 @@
+<script context="module">
+  import {preload, link_href, unlink} from "./profile";
+  export {preload};
+</script>
+
+<script>
+  export let linked_accounts = [];
+  export let sessions = [];
+</script>
+
 <h1 class="title">Profile</h1>
+
+<!-- linked_accounts -->
+<div class="notification">
+  <h2 class="subtitle">Linked Services</h2>
+  <table class="table">
+    <thead>
+      <tr>
+        <th>Service</th>
+        <th>User</th>
+        <th>Linking</th>
+      </tr>
+    </thead>
+    <tbody>
+      {#each linked_accounts as link}
+        <tr>
+          <td>
+            <i class="fab fa-{link.service_id}" />
+            <span>{link.service_name}</span>
+          </td>
+          <td>{link.name || 'n/a'}</td>
+          <td>
+            {#if link.name}
+              <button
+                class="button is-danger"
+                on:click={() => unlink(link.service_id)}>
+                Unlink
+              </button>
+            {:else}
+              <a class="button is-success" href={link_href(link)}>Link</a>
+            {/if}
+          </td>
+        </tr>
+      {/each}
+    </tbody>
+  </table>
+</div>
+
+<!-- sessions -->
+<div class="notification">
+  <h2 class="subtitle">Sessions</h2>
+  <table class="table">
+    <thead>
+      <tr>
+        <th>Last Login</th>
+        <th>User Agent</th>
+      </tr>
+    </thead>
+    <tbody>
+      {#each sessions as {created, user_agent}}
+        <tr>
+          <td>{created}</td>
+          <td>{user_agent}</td>
+        </tr>
+      {/each}
+    </tbody>
+  </table>
+</div>
---
+++ src/routes/profile.ts
@@ -0,0 +1,47 @@
+import {link_service, unlink_service} from "../auth";
+import {get_page} from "../request";
+import goto from "../goto";
+
+export async function preload(_, query: URLSearchParams) {
+  if (query.has("link") && query.has("code")) {
+    try {
+      const service = query.get("link");
+      const code = query.get("code");
+      await link_service(service, code);
+    } catch (err) {
+      console.log(err);
+      window.alert(err);
+    }
+    // this.redirect("/profile");
+    return;
+  }
+
+  const linked_accounts = (await get_page(`linked_accounts`, 1, 10)).pagedata;
+  const sessions = (await get_page(`sessions`, 1, 10)).pagedata;
+  return {linked_accounts, sessions};
+}
+
+export function link_href({service_id, client_id, auth_url, scope}): string {
+  const redirect_uri = encodeURIComponent(
+    `https://${location.host}/profile?link=${service_id}`,
+  );
+  return (
+    `${auth_url}` +
+    `?client_id=${client_id}` +
+    `&redirect_uri=${redirect_uri}` +
+    `&scope=${scope}` +
+    `&response_type=code` +
+    `&state=87d11950465e5937515c`
+  );
+}
+
+export async function unlink(service): Promise<void> {
+  try {
+    await unlink_service(service);
+    // TODO: remove nickname from service and redraw, without reroute.
+    goto("/");
+  } catch (err) {
+    console.log(err);
+    window.alert(err);
+  }
+}
