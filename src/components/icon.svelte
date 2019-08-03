b5cb7cf3cb3cbcea9be520b041b1c2e9404a3d5d
Regina Pfeifer <regina@mailbox.org>
2019-08-03 08:55:33 +0200

    Add profile page

---
+++ src/components/icon.svelte
@@ -0,0 +1,20 @@
+<script>
+  let class_name = "";
+  export let viewBox = "0 0 512 512";
+  export let d;
+  export {class_name as class};
+</script>
+
+<style>
+  svg {
+    height: 1em;
+  }
+</style>
+
+<svelte:options immutable={true} />
+
+<span class="icon {class_name}">
+  <svg aria-hidden="true" role="img" {viewBox}>
+    <path fill="currentColor" {d} />
+  </svg>
+</span>
--- src/router.js
+++ src/router.js
@@ -1,15 +1,18 @@
 import Index from "./routes/index.svelte";
-import SignUp from "./routes/signup.svelte";
 import Login from "./routes/login.svelte";
 import NotFound from "./routes/404.svelte";
+import Profile from "./routes/profile.svelte";
+import SignUp from "./routes/signup.svelte";
 
 export default function(path) {
   if (path === "/") {
     return Index;
-  } else if (path === "/signup") {
-    return SignUp;
   } else if (path === "/login") {
     return Login;
+  } else if (path === "/profile") {
+    return Profile;
+  } else if (path === "/signup") {
+    return SignUp;
   } else {
     return NotFound;
   }
