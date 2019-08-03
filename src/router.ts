25c3eaa3a7e2ac252ce18256004c70898fd60e88
Regina Pfeifer <regina@mailbox.org>
2019-08-03 21:28:05 +0200

    router: Support preload function

--- src/router.ts
+++ src/router.ts
@@ -6,16 +6,22 @@ import * as NotFound from "./routes/404.svelte";
 import * as Profile from "./routes/profile.svelte";
 import * as SignUp from "./routes/signup.svelte";
 
+type Params = Record<string, string>;
+type Props = Record<string, any>;
+
 interface Module {
   default: any;
+  preload?: (Params, URLSearchParams) => Promise<Props>;
 }
 
 interface Route {
   module: Module;
+  params: Params;
 }
 
 interface Page {
   page: any;
+  props: Props;
 }
 
 function select_module(path: string): Module {
@@ -34,11 +40,27 @@ function select_module(path: string): Module {
 
 export function select_route(path: string): Route {
   const module: Module = select_module(path);
-  return {module};
+  const params: Record<string, string> = {};
+  return {module, params};
 }
 
-export default function(location: Location): Page {
+export async function load_route(
+  route: Route,
+  query: URLSearchParams,
+): Promise<Page> {
+  const {module, params} = route;
+  const {default: page, preload} = module;
+  const props: Props = {};
+
+  if (preload) {
+    const preloaded: Props = await preload(params, query);
+    Object.assign(props, preloaded);
+  }
+
+  return {page, props};
+}
+
+export default function(location: Location): Promise<Page> {
   const route = select_route(location.pathname);
-  const page = route.module.default;
-  return {page};
+  return load_route(route, new URLSearchParams(location.search));
 }
