a86353d9183466c5d54743ddf9809def4ea92bf0
Regina Pfeifer <regina@mailbox.org>
2019-08-03 22:36:17 +0200

    Add some content to profile page

--- src/app.svelte
+++ src/app.svelte
@@ -2,14 +2,15 @@
   import PageHeader from "./components/page-header.svelte";
   import PageFooter from "./components/page-footer.svelte";
 
-  export let page;
+  export let page = null;
+  export let props = null;
 </script>
 
 <PageHeader />
 
 <section class="section">
   <div class="container">
-    <svelte:component this={page} />
+    <svelte:component this={page} {...props} />
   </div>
 </section>
 
