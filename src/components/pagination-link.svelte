56bea14aef94dc991ef1d928259c47ce66002b34
Regina Pfeifer <regina@mailbox.org>
2019-09-17 12:58:05 +0200

    Cleanup pagination code

---
+++ src/components/pagination-link.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let page;
+
+  $: href = (function() {
+    const query = new URLSearchParams(location.search);
+    query.set("page", page);
+    return "?" + query.toString();
+  })();
+</script>
+
+<li>
+  <a class="pagination-link" {href}>{page}</a>
+</li>
